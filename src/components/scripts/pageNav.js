113847c04bc5e0c78e699a94ed067e29606a54f7
Abolaji Disu <abolajiidiisu@gmail.com>
2023-08-16 20:28:48 +0100

    fixed scrollbar height

--- src/components/scripts/pageNav.js
+++ src/components/scripts/pageNav.js
@@ -3,14 +3,11 @@ import "../styles/bottomNav.scss";
 
 const BottomNav =  ({styles, link}) => {
 
-
-
     return (
         <Link to={link}>
         <div style={styles} className="bottom-nav">
              Swipe for more        &rarr;
         </div>
-        
         </Link>
     )
 }
